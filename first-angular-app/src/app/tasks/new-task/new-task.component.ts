ac7064cc2f25a836aab8babaa59b586f7cd1871e
Vanina Stancheva <vanina.v.stancheva@gmail.com>
2024-09-22 22:27:45 +0300

    adding a new task

---
+++ first-angular-app/src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-new-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css'
+})
+export class NewTaskComponent {
+@Input() newTaskId!: string;
+}
+
--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -20,5 +20,4 @@ export class UserComponent {
     this.select.emit(this.user.id);
   }
 
-  protected readonly MouseEvent = MouseEvent;
 }
